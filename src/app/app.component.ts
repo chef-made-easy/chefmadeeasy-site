1ca03d42477434f6a90412e264abfc618466a3f0
Mikhail Temkine <miktemk@gmail.com>
2019-03-30 00:28:25 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cme-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'chefmadeeasy-site';
+}
